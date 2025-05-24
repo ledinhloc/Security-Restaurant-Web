214fd86f9f8693bf23024059962e81a7676a3a55
ledinhloc <ledinhloc7@gmail.com>
2025-05-25 00:20:51 +0700

    add edit profile customer

--- src/main/java/com/restaurant/management/config/security/SecurityConfig.java
+++ src/main/java/com/restaurant/management/config/security/SecurityConfig.java
@@ -19,55 +19,77 @@ public class SecurityConfig {
     @Autowired
     private CustomUserDetailsService customUserDetailsService;
 
-    private final String[] ADMIN_ENDPOINTS = {
-            "/inventories/**",
-            "/tables",
-            "/employees/**",
-            "/customers",
-            "/dashboard",
-            "/shifts",
-            "/suppliers/**",
-            "/dishes",
-            "/recipes/**",
-            "/schedules",
-            "/reservations",
+    private static final String[] ADMIN_ENDPOINTS = {
+            "/inventories/**", "/tables", "/employees/**", "/customers",
+            "/dashboard", "/shifts", "/suppliers/**", "/dishes",
+            "/recipes/**", "/schedules", "/reservations"
     };
 
-    private final String[] PUBLIC_Endpoints = {
-            "/login",
-            "/request-otp",
-            "/register",
-            "/verify-otp",
-            "/forgot-password",
-            "/resources/**", "/css/**",
-            "/"
+    private static final String[] PUBLIC_ENDPOINTS = {
+            "/login", "/request-otp", "/register", "/verify-otp",
+            "/forgot-password", "/resources/**", "/css/**", "/"
     };
 
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .authorizeHttpRequests(authorize -> authorize
-                    .requestMatchers(PUBLIC_Endpoints).permitAll()
-                    .requestMatchers(ADMIN_ENDPOINTS).hasRole("ADMIN")
-                    .anyRequest().authenticated()
-            )
-            .formLogin(form -> form
-                    .loginPage("/login")
-                    .defaultSuccessUrl("/profile", false)
-                    .permitAll()
-            )
-            .logout(logout -> logout
-                .logoutUrl("/logout")
-                .logoutSuccessUrl("/login?logout")
-                .invalidateHttpSession(true)
-                .deleteCookies("JSESSIONID")
-            )
-            .userDetailsService(customUserDetailsService)
-            .sessionManagement(session -> session
-                    .maximumSessions(1)
-                    .maxSessionsPreventsLogin(false)
-            );
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(PUBLIC_ENDPOINTS).permitAll()
+                        .requestMatchers(ADMIN_ENDPOINTS).hasRole("ADMIN")
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/profile", false)
+                        .permitAll()
+                )
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/login?logout")
+                        .invalidateHttpSession(true)
+                        .deleteCookies("JSESSIONID")
+                )
+                .userDetailsService(customUserDetailsService)
+                .sessionManagement(session -> session
+                        .maximumSessions(1)
+                        .maxSessionsPreventsLogin(false)
+                );
+//                .headers(headers -> headers
+//                        .contentSecurityPolicy(csp -> csp
+//                                .policyDirectives(
+//                                        "default-src 'self'; " +
+//                                                "script-src 'self' https://cdn.tailwindcss.com; " +
+//                                                "style-src 'self' ; " +
+//                                                "img-src 'self' data:; " +
+//                                                "font-src 'self'; " +
+//                                                "connect-src 'self'; " +
+//                                                "frame-ancestors 'none'; " +
+//                                                "form-action 'self'; " +
+//                                                "base-uri 'self'"
+//                                )
+//                        )
+//                );
+
+//                .headers(headers -> headers
+//                        .contentSecurityPolicy(csp -> csp
+//                                .policyDirectives("default-src 'self'; script-src 'self'; style-src 'self'; img-src 'self' data:")
+//                        )
+//                );
+//            .headers(headers -> headers
+//                    .contentSecurityPolicy(csp -> csp
+//                            .policyDirectives(
+//                                    "default-src 'self'; " +
+//                                            "script-src 'self' https://cdn.jsdelivr.net; " +
+//                                            "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; " +
+//                                            "font-src 'self' https://fonts.gstatic.com; " +
+//                                            "img-src 'self' data:; " +
+//                                            "connect-src 'self' https://api.example.com; " +
+//                                            "frame-src 'none'; " +
+//                                            "object-src 'none';"
+//                            )
+//                    )
+//            );
 
         http.csrf(AbstractHttpConfigurer::disable);
         return http.build();
